349b3002c5534b6a9fc4de4545f0656761c9b281
bob <portenko@gmail.com>
2020-11-06 21:23:43 +0500

    add seeders for articles and tags

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //\App\Models\User::factory(10)->create();
+        \App\Models\Article::factory(20)->create();
+        \App\Models\Tag::factory(20)->create();
+        $tags = \App\Models\Tag::all();
+        \App\Models\Article::all()->each(function ($article) use ($tags) {
+            $article->tags()->attach(
+                $tags->random(rand(1, 3))->pluck('id')->toArray()
+            );
+        });
     }
 }
