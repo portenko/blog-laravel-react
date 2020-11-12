731f03e3c6aa508095dd087d3187ca0319b182db
bob <portenko@gmail.com>
2020-11-12 09:32:46 +0500

    remove mix.react().sass()

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,3 @@ const mix = require('laravel-mix');
 
 // Module not found: Error: Can't resolve 'fs' in 'node_modules\*'
 mix.webpackConfig({ node: { fs: 'empty' }});
-
-mix.react('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
